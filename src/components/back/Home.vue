0dc771e03e75ccc7e0ca7fe049070c91cbce591f
wang <2041409629@qq.com>
2020-08-24 15:51:39 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/App.vue
    #	src/components/back/404.vue
    #	src/components/back/Login.vue
    #	src/main.js
    #	src/router/index.js

--- src/components/back/Home.vue
+++ src/components/back/Home.vue
@@ -105,33 +105,33 @@ export default {
     handlerCommand: function (command) {
       this.$router.push({name: command})
     },
-      // 全屏事件
-      handleFullScreen:function(){
-          let element = document.documentElement;
-          if (this.fullscreen) {
-              if (document.exitFullscreen) {
-                  document.exitFullscreen();
-              } else if (document.webkitCancelFullScreen) {
-                  document.webkitCancelFullScreen();
-              } else if (document.mozCancelFullScreen) {
-                  document.mozCancelFullScreen();
-              } else if (document.msExitFullscreen) {
-                  document.msExitFullscreen();
-              }
-          } else {
-              if (element.requestFullscreen) {
-                  element.requestFullscreen();
-              } else if (element.webkitRequestFullScreen) {
-                  element.webkitRequestFullScreen();
-              } else if (element.mozRequestFullScreen) {
-                  element.mozRequestFullScreen();
-              } else if (element.msRequestFullscreen) {
-                  // IE11
-                  element.msRequestFullscreen();
-              }
-          }
-          this.fullscreen = !this.fullscreen;
+    // 全屏事件
+    handleFullScreen: function () {
+      let element = document.documentElement
+      if (this.fullscreen) {
+        if (document.exitFullscreen) {
+          document.exitFullscreen()
+        } else if (document.webkitCancelFullScreen) {
+          document.webkitCancelFullScreen()
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen()
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen()
+        }
+      } else {
+        if (element.requestFullscreen) {
+          element.requestFullscreen()
+        } else if (element.webkitRequestFullScreen) {
+          element.webkitRequestFullScreen()
+        } else if (element.mozRequestFullScreen) {
+          element.mozRequestFullScreen()
+        } else if (element.msRequestFullscreen) {
+          // IE11
+          element.msRequestFullscreen()
+        }
       }
+      this.fullscreen = !this.fullscreen
+    }
   }
 }
 </script>
